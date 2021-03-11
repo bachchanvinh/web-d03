41fb175b95dc0c06669874eec1a4ea8795f55f02
bachchanvinh <vinhbach19@gmail.com>
2021-03-11 09:42:44 +0700

    Finished

--- Lession-1-js/Bai2.js
+++ Lession-1-js/Bai2.js
@@ -1,8 +1,11 @@
 function sumZero(arrSorted) {
-    let res = []
-    for (let i = 0; arrSorted[i] >= 0; i++) {
-        if (arrSorted[i] + arrSorted) {
-
+    for (let i = 0; arrSorted[i] < 0; i++) {
+        let check = arrSorted.find(element => element === -arrSorted[i])
+        if (check !== undefined) {
+            return [-check, check]
         }
     }
+    return undefined
 }
+
+console.log(sumZero([-4, -2, -1, 0, 1, 2, 3, 5]))
